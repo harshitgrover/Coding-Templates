b4d8b29784a1c74d698ce935dc9ac3c4aefba5f5
Harshit Grover <harshitgrover007@gmail.com>
2026-01-29 04:26:28 +0530

    template with common C++ definitions and macros

---
+++ boilerplate/template.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define dbl long double
+#define str string
+#define elif else if
+#define fi(i, a, b) for (ll i = a; i < b; i++)
+#define fd(i, a, b) for (ll i = a; i > b; i--)
+#define input(a) for (ll _ = 0; _ < a.size(); _++) cin >> a[_]
+#define iter(a, b) for (auto & b: a)
+#define iterp(a) for (auto & [it1, it2]: a)
+#define prinv(a) iter(a, _) {cout << _ << " ";} cout << endl
+#define prin(a) cout << a << " "
+#define print(a) cout << a << endl
+#define prinp(a) {prin(a.first); print(a.second);}
+#define nline() cout << endl
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define mp make_pair
+#define emb emplace_back
+#define pushb push_back
+#define pushf push_front
+#define popb pop_back
+#define popf pop_front
+#define fs first
+#define sc second
+#define idk print("ok")
+#define yes print("YES")
+#define no print("NO")
+#define vll vector<ll>
+#define vint vector<int>
+#define vvll vector<vll>
+#define pll pair<ll, ll>
+#define sll set<ll>
+#define vbool vector<bool>
+#define vset vector<sll>
+#define vstr vector<str>
+#define vchar vector<char>
+#define vpair vector<pair<ll, ll>>
+#define vdbl vector < long double >
+#define lcm(a,b) (((a)/gcd(a,b))*b)
+
+const ll inf = 1e16;
+const ll mod1 = 1e9 + 7;
+const ll mod2 = 998244353;
+

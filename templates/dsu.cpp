650f24b7fb21cf5239949b104532e4d83a94c1dc
Harshit Grover <harshitgrover007@gmail.com>
2026-03-28 11:38:01 +0530

    Update dsu.cpp

--- templates/dsu.cpp
+++ templates/dsu.cpp
@@ -13,7 +13,7 @@ public:
     long long findp(long long node){
         return node==parent[node]?node:parent[node]=findp(parent[node]);
     }
-    void unionr(long long u,long long v){
+    void unionr(long long u, long long v){
         long long pu=findp(u),pv=findp(v);
         if(pu==pv) return;
         if(rank[pu]<rank[pv]) parent[pu]=pv,sz[pv]+=sz[pu];
@@ -23,15 +23,15 @@ public:
     long long getsize(long long u){
         return sz[findp(u)];
     }
-    bool iscon(long long u,long long v){
+    bool iscon(long long u, long long v){
         return findp(u)==findp(v);
     }
     vector<long long> getrank(){
         return rank;
     }
-    long long countsets(long long j){
+    long long countsets(){
         long long ans=0;
-        for(long long i=j;i<(long long)parent.size()-1+j;i++) if(parent[i]==i) ans++;
-        return ans;
+        for(long long i=0; i<(long long)parent.size(); i++) if(parent[i]==i) ans++;
+        return ans-1;
     }
 };
